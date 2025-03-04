67a8b2edc9d2604a09382ffae179c73967ed9b62
Emilio Ferraro <eferraro@quadiontech.com>
2025-03-03 23:04:34 -0300

    installa y usa cors + lock update

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
+import cors from 'cors'
 import express from 'express'
 import { ApiRoutes } from './router/apiRoutes'
 
 const app = express()
+//Permitir cors para todos los origenes
+//app.use(cors());
+
+app.use(cors({ origin: 'http://localhost:5173' }))
 const port = process.env.PORT || 8000
 
+//La aplicacion con Express se le pasa como parametro a ApiRoutes
 ApiRoutes(app)
 
 app.listen(port, () => {
